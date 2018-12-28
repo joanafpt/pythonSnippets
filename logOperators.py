c71f48cf848d8cdecd3e20b1d3eed745f322ac7f
Joana <joanajteixeira@outlook.com>
2018-12-28 09:48:11 +0000

    #Prints four question marks using a loop

---
+++ logOperators.py
@@ -0,0 +1,10 @@
+#Logical operators
+from cs50 import get_char
+#Prompt user for an answer
+c = get_char("answer: ")
+
+#Check answer
+if c == "Y" or c == "y":
+    print("yes")
+elif c == "N" or c == "n":
+    print("no")
